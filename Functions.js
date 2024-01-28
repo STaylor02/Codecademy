44364610508fcce1c14a12bf12a3d4dfe76fd1dc
Scott Taylor <135455232+STaylor02@users.noreply.github.com>
2024-01-28 02:41:09 +0000

    Functions
    Codecademy Functions Sector

---
+++ Functions.js
@@ -0,0 +1,32 @@
+getReminder();
+
+function getReminder() {
+  console.log("Water the plants.");
+};
+
+greetInSpanish();
+
+function greetInSpanish() {
+  console.log("Buenas tardes");
+};
+
+
+function sayThanks() {
+    console.log("Thank you for your purchase! We appreciate your business.");
+  };
+  
+  sayThanks();
+  sayThanks();
+  sayThanks();
+
+  
+
+  function sayThanks(name) {
+    console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
+  };
+  
+  sayThanks("Cole");
+
+
+
+  
