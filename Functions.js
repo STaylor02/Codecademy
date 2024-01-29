9433476d01507e3cde09412156053f3fc841f48d
Scott Taylor <135455232+STaylor02@users.noreply.github.com>
2024-01-29 03:36:41 +0000

    Update Functions
    Added Default Parameters & Return Function

--- Functions.js
+++ Functions.js
@@ -1,3 +1,4 @@
+//Function Declarations
 getReminder();
 
 function getReminder() {
@@ -11,22 +12,37 @@ function greetInSpanish() {
 };
 
 
+//Calling a Function
 function sayThanks() {
-    console.log("Thank you for your purchase! We appreciate your business.");
-  };
+  console.log("Thank you for your purchase! We appreciate your business.");
+};
   
-  sayThanks();
-  sayThanks();
-  sayThanks();
-
-  
-
-  function sayThanks(name) {
-    console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
-  };
-  
-  sayThanks("Cole");
-
-
+sayThanks();
+sayThanks();
+sayThanks();
 
   
+//Parameters and Arguments
+function sayThanks(name) {
+  console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
+};
+  
+sayThanks("Cole");
+
+
+//Default Parameters
+function makeShoppingList(item1 = "milk", item2 = "bread", item3 = "eggs"){
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+}
+
+
+//Return function
+function monitorCount(rows, columns) {
+  return rows * columns;
+};
+
+const numOfMonitors = monitorCount(5, 4);
+
+console.log(numOfMonitors);
