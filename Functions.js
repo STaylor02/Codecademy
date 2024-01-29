0e6c5eab5156896b409a9153dfbacbca923082c1
Scott Taylor <135455232+STaylor02@users.noreply.github.com>
2024-01-29 03:54:51 +0000

    Updated Functions
    Added Arrow functions & Concise body arrow functions

--- Functions.js
+++ Functions.js
@@ -74,3 +74,22 @@ const plantNeedsWater = function(day) {
 };
 
 console.log(plantNeedsWater("Tuesday"));
+
+
+
+//Arrow Functions
+const plantNeedsWater = (day) => {
+  if (day === 'Wednesday') {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+
+//Concise Body Arrow Functions 
+const plantNeedsWater = day => day === "Wednesday" ? true : false;
+
+
+
