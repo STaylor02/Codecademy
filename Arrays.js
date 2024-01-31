1a604f1fdf6d3a437691a51a88fce8496d252e23
Scott Taylor <135455232+STaylor02@users.noreply.github.com>
2024-01-31 16:47:55 +0000

    Arrays
    Codecademy Arrays Sector

---
+++ Arrays.js
@@ -0,0 +1,23 @@
+//Arrays
+const hobbies = ["Hiking", "Gym", "Gaming"];
+console.log(hobbies);
+
+
+
+//Accessing Elements
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+var listItem = famousSayings[0];
+console.log(listItem); //logs the first stored variable
+console.log(famousSayings[2]); //logs the third stored variable
+console.log(famousSayings[3]); //tries to log an undefined variable
+
+
+
+//Update Elements 
+let groceryList = ['bread', 'tomatoes', 'milk'];
+
+groceryList[1] = "avocados";
+
+
+
