05cd409ea334e2fac2220648ee43a622596a7659
Scott Taylor <135455232+STaylor02@users.noreply.github.com>
2024-02-01 00:37:50 +0000

    Began loop sector
    - Added For Loop
    - Added Looping in reverse
    - Added Looping through arrays

---
+++ Loops.js
@@ -0,0 +1,22 @@
+//Loops
+//The For Loop
+for (let num = 5; num < 11; num++) {
+    console.log(num);
+}
+
+
+
+//Looping in Reverse
+for (let counter = 3; counter >= 0; counter--){
+    console.log(counter);
+}
+
+
+
+//Looping through Arrays
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+// Write your code below
+for (let i = 0; i < vacationSpots.length; i++) {
+  console.log("I would love to visit " + vacationSpots[i]);
+}
