8e1baf914174f1108461189678628f39569ae580
Scott Taylor <135455232+STaylor02@users.noreply.github.com>
2024-02-03 19:16:37 +0000

    Completed Codecademy Dog Years Project
    - Used simple code to create a human to dog years calculator.

---
+++ Dog Years Project.js
@@ -0,0 +1,16 @@
+//creates a variable with my age
+var myAge = 21;
+var myName = "Scott".toLowerCase();
+
+// creates a new variable with early years
+var earlyYears = 2;
+earlyYears*= 10.5;
+
+laterYears = myAge - 2;
+laterYears*= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+var myAgeInDogYears = earlyYears + laterYears;
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`)
